6d4b5f150e7d9f93227d75ce191fea175d307f2f
Bruno Bortolli <brunobortolli@outlook.com>
2023-09-03 21:16:12 -0300

    gardening

--- src/ruler/models.clj
+++ src/ruler/models.clj
@@ -4,25 +4,25 @@
   (fn [k _rule _data] k))
 
 (defmethod data-key-validation :type
-  [k rule data]
+  [_k rule data]
   (let [rule-type (:type rule)
         val (get data (:key rule))
         err (not (instance? rule-type val))]
     (when err "Erro :type")))
 
 (defmethod data-key-validation :req
- [k rule data]
+ [_k rule data]
  (let [err (and (:req rule) (nil? (get data (:key rule))))]
    (when err "Erro :req")))
 
 (defmethod data-key-validation :req-depends
- [k rule data]
+ [_k rule data]
  (let [every-deps? (every? some? (vals (select-keys data (:req-depends rule))))
        err (and (not every-deps?) (nil? (get data (:key rule))))]
    (when err "Erro :req-depends")))
 
 (defmethod data-key-validation :req-fn
- [k rule data]
+ [_k rule data]
  (let [req-fn (:req-fn rule)
        err (and (req-fn data) (nil? (get data (:key rule))))]
    (when err "Erro :req-fn")))
@@ -31,27 +31,27 @@
   (not (>= max val min)))
 
 (defmethod data-key-validation :min
-  [k rule data]
+  [_k rule data]
   (let [err  (limits-validation-error (:min rule) Double/POSITIVE_INFINITY (get data (:key rule)))]
     (when err "Erro :min")))
 
 (defmethod data-key-validation :max
-  [k rule data]
+  [_k rule data]
   (let [err  (limits-validation-error Double/NEGATIVE_INFINITY (:max rule) (get data (:key rule)))]
     (when err "Erro :max")))
 
 (defmethod data-key-validation :min-length
-  [k rule data]
+  [_k rule data]
   (let [err  (limits-validation-error (:min-length rule) Integer/MAX_VALUE (count (get data (:key rule))))]
     (when err "Erro :min-length")))
 
 (defmethod data-key-validation :max-length
-  [k rule data]
+  [_k rule data]
   (let [err  (limits-validation-error -1 (:max-length rule) (count (get data (:key rule))))]
     (when err "Erro :max-length")))
 
 (defmethod data-key-validation :length
-  [k rule data]
+  [_k rule data]
   (let [val  (get data (:key rule))
         length (:length rule)
         val' (if (string? val) (count val) val)
@@ -59,7 +59,7 @@
     (when err "Erro :length")))
 
 (defmethod data-key-validation :contains
-  [k rule data]
+  [_k rule data]
   (let [val (get data (:key rule))
         values (:contains rule)
         err (not (contains? (set values) val))]
@@ -69,13 +69,13 @@
   (not (fn val)))
 
 (defmethod data-key-validation :format
-  [k rule data]
+  [_k rule data]
   (let [fmt (:format rule)
         err (format-validation-error (partial re-matches fmt) (get data (:key rule)))]
     (when err "Erro :format")))
 
 (defmethod data-key-validation :format-fn
-  [k rule data]
+  [_k rule data]
   (let [format-fn (:format-fn rule)
         err (format-validation-error format-fn (get data (:key rule)))]
     (when err "Erro :format-fn")))
