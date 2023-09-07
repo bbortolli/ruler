bd18db65fc0f9477d9656314fa965bc25e1666f0
Bruno Bortolli <brunobortolli@outlook.com>
2023-09-07 17:19:26 -0300

    updating project for future clojars deploys

--- project.clj
+++ project.clj
@@ -1,7 +1,8 @@
 (defproject ruler "0.1.0-SNAPSHOT"
   :description "Data validation pure functionally library for Clojure"
-  :url "https://github.com/bbortolli/clj-ruler"
+  :url "https://github.com/bbortolli/ruler"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
   :dependencies [[org.clojure/clojure "1.11.1"]]
+  :scm {:name "git" :url "https://github.com/bbortolli/ruler"}
   :repl-options {:init-ns ruler.core})
