27f29ef3feb78ff3b54d6e23229a3cd4ce77a91e
Bruno Bortolli <brunobortolli@outlook.com>
2023-09-08 23:39:02 -0300

    feat: add options at models and validate

--- src/ruler/core.clj
+++ src/ruler/core.clj
@@ -43,23 +43,34 @@
         validate-fn #(models/key-validation % rule data')]
     (remove nil? (map validate-fn keys))))
 
+(defn data->opts-errors [{:keys [model opts]} data]
+  (when-let [opt-keys (keys opts)]
+    (let [validate-fn #(models/opt-key-validation % model opts data)]
+      (remove nil? (map validate-fn opt-keys)))))
+
 (defn- data->errors [model data injection]
-  (let [validate-fn #(data->rule-errors % data injection)]
-    (remove nil? (flatten (map validate-fn model)))))
+  (let [validate-fn #(data->rule-errors % data injection)
+        rule-errors (remove nil? (flatten (map validate-fn (:model model))))
+        opts-errors (remove nil? (data->opts-errors model data))]
+    {:rule-errors rule-errors
+     :opts-errors (apply merge opts-errors)}))
 
 (defn- data->valid-model? [model data injection]
   (let [errors (data->errors model data injection)]
-    (empty? errors)))
+    (and (empty? (:rule-errors errors))
+         (empty? (:opts-errors errors)))))
 
 (defn ^:no-doc validate* [model data injection]
   (data->valid-model? model data injection))
 
 (defn ^:no-doc describe* [model data injection]
   (let [err (data->errors model data injection)
-        grouped (group-by :key err)
+        rule-errors (:rule-errors err)
+        opts-errors (:opts-errors err)
+        grouped (group-by :key rule-errors)
         preds-reducer (fn [a k v]
                         (assoc a k (mapv :pred v)))]
-    (reduce-kv preds-reducer {} grouped)))
+    (reduce-kv preds-reducer opts-errors grouped)))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; Public api.
@@ -68,11 +79,13 @@
 (defn defmodel
   "Define a new model with a set of rules identified as 'kw'."
   {:added "1.0"}
-  [kw model]
-  (assert (keyword? kw) "Model identifier should be a keyword.")
-  (assert (seq model) "Model can not be empty.")
-  (rules/validate-all-rules model)
-  (create-model! kw model))
+  ([kw model]
+   (defmodel kw model nil))
+  ([kw model opts]
+   (assert (keyword? kw) "Model identifier should be a keyword.")
+   (assert (seq model) "Model can not be empty.")
+   (rules/validate-all-rules model)
+   (create-model! kw {:model model :opts opts})))
 
 (defn valid?
   "Validate input data following the rules defined in the model identified as 'k'."
--- test/ruler/core_test.clj
+++ test/ruler/core_test.clj
@@ -15,10 +15,14 @@
         account-type (:account-type injection)]
     (= "premium" account-type)))
 
-(def test-model [{:key :number :type Integer :req true :min 1 :max 10}
-                 {:key :string :type String :req-depends [:number] :min-length 2 :max-length 4}
-                 {:key :opt :type String :req-fn (fn [d] (= 10 (:number d))) :format #"[a-z]{2}"}
-                 {:key :card :type String :req-fn validator-with-injection :format #"\d{4}-\d{4}-\d{4}-\d{4}"}])
+(def test-model
+  {:model [{:key :number :type Integer :req true :min 1 :max 10}
+           {:key :string :type String :req-depends [:number] :min-length 2 :max-length 4}
+           {:key :opt :type String :req-fn (fn [d] (= 10 (:number d))) :format #"[a-z]{2}"}
+           {:key :card :type String :req-fn validator-with-injection :format #"\d{4}-\d{4}-\d{4}-\d{4}"}]})
+
+(def test-model-with-opts
+  (assoc test-model :opts {:extra-keys? false}))
 
 (deftest validate*-test
   (let [db-injection {:account-type "premium"}]
@@ -26,7 +30,8 @@
       (is (true? (core/validate* test-model {:number 1 :string "ab"} nil)))
       (is (true? (core/validate* test-model {:number 5 :string "abc"} nil)))
       (is (true? (core/validate* test-model {:number 10 :string "abcd" :opt "si"} nil)))
-      (is (true? (core/validate* test-model {:number 7 :string "abcd" :card "1234-1234-1234-1234"} db-injection))))
+      (is (true? (core/validate* test-model {:number 7 :string "abcd" :card "1234-1234-1234-1234"} db-injection)))
+      (is (true? (core/validate* test-model {:number 10 :string "abcd" :opt "si" :extra "ok"} nil))))
     (testing "Invalid models"
       (is (false? (core/validate* test-model {:number "a"} nil)) "Invalid type for :number")
       (is (false? (core/validate* test-model {:number -9} nil)) "Invalid min value for :number")
@@ -36,7 +41,8 @@
       (is (false? (core/validate* test-model {:number 5 :string "a"} nil)) "Invalid min-length for :string")
       (is (false? (core/validate* test-model {:number 5 :opt "a"} nil)) "Invalid format for :opt")
       (is (false? (core/validate* test-model {:number 5 :opt "abcd"} nil)) "Invalid format for :opt")
-      (is (false? (core/validate* test-model {:number 6 :string "ab"} db-injection)) "Missing required key :card-number req-fn with injection"))))
+      (is (false? (core/validate* test-model {:number 6 :string "ab"} db-injection)) "Missing required key :card-number req-fn with injection")
+      (is (false? (core/validate* test-model-with-opts {:number 6 :string "ab" :extra 12345} nil)) "Extra key"))))
 
 (deftest describe*-test
   (testing "Described valid models"
@@ -46,5 +52,6 @@
     (is (= {:number [:max] :string [:req-depends]} (core/describe* test-model {:number 999} nil)) "invalid number limit max")
     (is (= {:string [:type]} (core/describe* test-model {:number 7 :string 123} nil)) "invalid string type")
     (is (= {:string [:min-length]} (core/describe* test-model {:number 7 :string "1"} nil)) "invalid min-length")
-    (is (= {:string [:max-length]} (core/describe* test-model {:number 7 :string "1234567890"} nil)) "invalid max-length"))
+    (is (= {:string [:max-length]} (core/describe* test-model {:number 7 :string "1234567890"} nil)) "invalid max-length")
+    (is (= {:ruler/extra-keys? [:extra]} (core/describe* test-model-with-opts {:number 6 :string "ab" :extra 12345} nil)) "invalid extra key"))
   (testing "Described invalid models"))
