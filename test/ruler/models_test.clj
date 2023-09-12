eb8f522f807d9cff3f5f5d4007fb6b18600534ed
Bruno Bortolli <bbortolli2@hotmail.com>
2023-09-12 18:49:44 -0300

    feat: implement logic and tests

--- test/ruler/models_test.clj
+++ test/ruler/models_test.clj
@@ -9,6 +9,8 @@
    :req true
    :req-depends [:bla]
    :req-fn (fn [_] true)
+   :excluded [:make-excluded]
+   :excluded-fn (fn [_] false)
    :min-length 3
    :max-length 10
    :length 6
@@ -22,6 +24,8 @@
    :req true
    :req-depends [:bla]
    :req-fn (fn [_] true)
+   :excluded [:test]
+   :excluded-fn (fn [_] true)
    :min -9999
    :max 9999})
 
@@ -36,32 +40,36 @@
 (deftest key-validation-test
 
   (testing "Valid data value"
-    (is (nil? (models/key-validation :type test-rule test-data )) ":type")
-    (is (nil? (models/key-validation :req test-rule test-data )) ":req")
-    (is (nil? (models/key-validation :req-depends test-rule test-data )) ":req-depends")
-    (is (nil? (models/key-validation :req-fn test-rule test-data )) ":req-fn")
-    (is (nil? (models/key-validation :min-length test-rule test-data )) ":min-length")
-    (is (nil? (models/key-validation :max-length test-rule test-data )) ":max-length")
-    (is (nil? (models/key-validation :min test-rule2 test-data2)) ":min")
-    (is (nil? (models/key-validation :max test-rule2 test-data2)) ":max")
-    (is (nil? (models/key-validation :length test-rule test-data )) ":length")
-    (is (nil? (models/key-validation :contains test-rule test-data )) ":contains")
-    (is (nil? (models/key-validation :format test-rule test-data )) ":format")
-    (is (nil? (models/key-validation :format-fn test-rule test-data )) ":format-fn"))
+    (is (nil? (models/key-validation :type        test-rule test-data))   ":type")
+    (is (nil? (models/key-validation :req         test-rule test-data))   ":req")
+    (is (nil? (models/key-validation :req-depends test-rule test-data))   ":req-depends")
+    (is (nil? (models/key-validation :req-fn      test-rule test-data))   ":req-fn")
+    (is (nil? (models/key-validation :excluded    test-rule test-data))   ":excluded")
+    (is (nil? (models/key-validation :excluded-fn test-rule test-data))   ":excluded-fn")
+    (is (nil? (models/key-validation :min-length  test-rule test-data))   ":min-length")
+    (is (nil? (models/key-validation :max-length  test-rule test-data))   ":max-length")
+    (is (nil? (models/key-validation :min         test-rule2 test-data2)) ":min")
+    (is (nil? (models/key-validation :max         test-rule2 test-data2)) ":max")
+    (is (nil? (models/key-validation :length      test-rule test-data))   ":length")
+    (is (nil? (models/key-validation :contains    test-rule test-data))   ":contains")
+    (is (nil? (models/key-validation :format      test-rule test-data))   ":format")
+    (is (nil? (models/key-validation :format-fn   test-rule test-data))   ":format-fn"))
 
   (testing "Invalid data value"
-    (is (= {:key :test :pred :type} (models/key-validation :type test-rule {:test 1.1})) ":type")
-    (is (= {:key :test :pred :req} (models/key-validation :req test-rule {})) ":req")
-    (is (= {:key :test :pred :req-depends} (models/key-validation :req-depends test-rule {:bla true})) ":req-depends")
-    (is (= {:key :test :pred :req-fn} (models/key-validation :req-fn test-rule {})) ":req-fn")
-    (is (= {:key :test :pred :min-length} (models/key-validation :min-length test-rule {:test "a"})) ":min-length")
-    (is (= {:key :test :pred :max-length} (models/key-validation :max-length test-rule {:test "abcdefghijklmnopqrstuvwxyz"})) ":max-length")
-    (is (= {:key :test :pred :min} (models/key-validation :min test-rule2 {:test -77777777})) ":min")
-    (is (= {:key :test :pred :max} (models/key-validation :max test-rule2 {:test 999999999})) ":max")
-    (is (= {:key :test :pred :length} (models/key-validation :length test-rule {:test "ble"})) ":length")
-    (is (= {:key :test :pred :contains} (models/key-validation :contains test-rule {:test "bli"})) ":contains")
-    (is (= {:key :test :pred :format} (models/key-validation :format test-rule {:test "blu"})) ":format")
-    (is (= {:key :test :pred :format-fn} (models/key-validation :format-fn (assoc test-rule :format-fn (fn [_] false)) {:test "teste"})) ":format-fn")))
+    (is (= {:key :test :pred :type}        (models/key-validation :type        test-rule   {:test 1.1})) ":type")
+    (is (= {:key :test :pred :req}         (models/key-validation :req         test-rule   {})) ":req")
+    (is (= {:key :test :pred :req-depends} (models/key-validation :req-depends test-rule   {:bla true})) ":req-depends")
+    (is (= {:key :test :pred :req-fn}      (models/key-validation :req-fn      test-rule   {})) ":req-fn")
+    (is (= {:key :test :pred :excluded}    (models/key-validation :excluded    test-rule   {:test "1" :make-excluded true})) ":excluded")
+    (is (= {:key :test :pred :excluded-fn} (models/key-validation :excluded-fn test-rule2  {:test "1"})) ":excluded-fn")
+    (is (= {:key :test :pred :min-length}  (models/key-validation :min-length  test-rule   {:test "a"})) ":min-length")
+    (is (= {:key :test :pred :max-length}  (models/key-validation :max-length  test-rule   {:test "abcdefghijklmnopqrstuvwxyz"})) ":max-length")
+    (is (= {:key :test :pred :min}         (models/key-validation :min         test-rule2  {:test -77777777})) ":min")
+    (is (= {:key :test :pred :max}         (models/key-validation :max         test-rule2  {:test 999999999})) ":max")
+    (is (= {:key :test :pred :length}      (models/key-validation :length      test-rule   {:test "ble"})) ":length")
+    (is (= {:key :test :pred :contains}    (models/key-validation :contains    test-rule   {:test "bli"})) ":contains")
+    (is (= {:key :test :pred :format}      (models/key-validation :format      test-rule   {:test "blu"})) ":format")
+    (is (= {:key :test :pred :format-fn}   (models/key-validation :format-fn   (assoc test-rule :format-fn (fn [_] false)) {:test "teste"})) ":format-fn")))
 
 (deftest opt-key-validation-test
 
