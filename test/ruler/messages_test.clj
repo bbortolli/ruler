420932c38aebe1f6f09a9ff130b0c275ebe56449
Bruno Bortolli <bbortolli2@hotmail.com>
2023-09-16 00:00:09 -0300

    ops

--- test/ruler/messages_test.clj
+++ test/ruler/messages_test.clj
@@ -47,29 +47,29 @@
 
 
   (testing "With custom functions for every :pred"
-    (is (= "Invalid type for field :f. Expected Integer, received String."
+    (is (= "Custom xD"
            (messages/pred-msg :type        {:key :f :type Integer}              {:f "123"} custom-messages)))
-    (is (= "Missing required field :f."
+    (is (= "Custom xD"
            (messages/pred-msg :req         {:key :f :req true}                  {:f "123"} custom-messages)))
-    (is (= "Missing required field :f."
+    (is (= "Custom xD"
            (messages/pred-msg :req-depends {:key :f :req-depends [:f2]}         {:f2 "123"} custom-messages)))
-    (is (= "Missing required field :f."
+    (is (= "Custom xD"
            (messages/pred-msg :req-fn      {:key :f :req-fn (fn [_] true)}      {:f "123"} custom-messages)))
-    (is (= "Disallowed field :f."
+    (is (= "Custom xD"
            (messages/pred-msg :excluded    {:key :f :excluded [:f2]}            {:f "123" :f2 "123"} custom-messages)))
-    (is (= "Disallowed field :f."
+    (is (= "Custom xD"
            (messages/pred-msg :excluded-fn {:key :f :excluded-fn (fn [_] true)} {:f "123"} custom-messages)))
-    (is (= "Invalid minimum value for :f. Minimum is 1, received 0."
+    (is (= "Custom xD"
            (messages/pred-msg :min         {:key :f :min 1}                     {:f 0} custom-messages)))
-    (is (= "Invalid maximum value for :f. Maximum is 5, received 6."
+    (is (= "Custom xD"
            (messages/pred-msg :max         {:key :f :max 5}                     {:f 6} custom-messages)))
-    (is (= "Invalid minimum length for :f. Minimum is 2, received 1."
+    (is (= "Custom xD"
            (messages/pred-msg :min-length  {:key :f :min-length 2}              {:f "1"} custom-messages)))
-    (is (= "Invalid maximum length for :f. Maximum is 4, received 6."
+    (is (= "Custom xD"
            (messages/pred-msg :max-length  {:key :f :max-length 4}              {:f "123456"} custom-messages)))
-    (is (= "Invalid possible values for :f. Expected bla, ble, received 123."
+    (is (= "Custom xD"
            (messages/pred-msg :contains    {:key :f :contains ["bla" "ble"]}    {:f "123"} custom-messages)))
-    (is (= "Invalid format for :f."
+    (is (= "Custom xD"
            (messages/pred-msg :format      {:key :f :format #""}                {:f "123"} custom-messages)))
-    (is (= "Invalid format function for :f."
+    (is (= "Custom xD"
            (messages/pred-msg :format-fn   {:key :f :format-fn (fn [_] false)}  {:f "123"} custom-messages)))))
